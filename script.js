8081da3796f3227fc275c08a4696a97d8fa4ff6c
Varun Kandhari <75046190+Varun612@users.noreply.github.com>
2025-05-11 04:07:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function stringChop(str, size) 
+function stringChop(str, size) {
   if (typeof str !== 'string') str = '';
  size = parseInt(size, 10);
-      if (isNaN(size) || size <= 0) {
-        return [];
-      }
+      // if (isNaN(size) || size <= 0) {
+      //   return [];
+      // }
    const res = [];
   // Walk through the string in steps of `size`
   for (let i = 0; i < str.length; i += size) {
